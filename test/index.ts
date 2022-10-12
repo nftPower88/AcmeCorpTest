76a52860de99f9ba0d1374b70fbffc6e21057d82
nftPower88 <babybear88yy@gmail.com>
2022-10-12 06:53:09 +0200

    update contract for library

--- test/index.ts
+++ test/index.ts
@@ -1,19 +1,38 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+describe("WidgetsFactory", function () {
+  it("InventoryManagement: manager add 10 stock success", async function () {
+    const widgetsFactory = await ethers.getContractFactory("WidgetsFactory");
+    const factory = await widgetsFactory.deploy();
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+    // add manager
+    await factory._addManager('0x0Fc84192A7F161Bff462c03359C543ffE0B3B5D8');
+    expect(await factory._managers('0x0Fc84192A7F161Bff462c03359C543ffE0B3B5D8')).to.equal(true);
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+    await factory.InventoryManagement('0x0Fc84192A7F161Bff462c03359C543ffE0B3B5D8', 10);
+    expect(await factory._getAmount()).to.equal(10);
+  });
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+  it("InventoryManagement: manager add 0 stock fail", async function () {
+    const widgetsFactory = await ethers.getContractFactory("WidgetsFactory");
+    const factory = await widgetsFactory.deploy();
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+    // add manager
+    await factory._addManager('0x0Fc84192A7F161Bff462c03359C543ffE0B3B5D8');
+    expect(await factory._managers('0x0Fc84192A7F161Bff462c03359C543ffE0B3B5D8')).to.equal(true);
+
+    await expect(factory.InventoryManagement('0x0Fc84192A7F161Bff462c03359C543ffE0B3B5D8', 0)).to.be.revertedWith("The factory has not produced the stock");
+  });
+
+  it("InventoryManagement: customer add 10 stock fail", async function () {
+    const widgetsFactory = await ethers.getContractFactory("WidgetsFactory");
+    const factory = await widgetsFactory.deploy();
+
+    // add manager
+    await factory._addManager('0x0Fc84192A7F161Bff462c03359C543ffE0B3B5D8');
+    expect(await factory._managers('0x0Fc84192A7F161Bff462c03359C543ffE0B3B5D8')).to.equal(true);
+
+    await expect(factory.InventoryManagement('0xBfE59ff420cB2045f0F4e9CcA266415a485f1F48', 10)).to.be.revertedWith("Customer can not alter stock in inventory");
   });
 });
